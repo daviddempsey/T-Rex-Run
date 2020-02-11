1134c8c6d88eb6040c2ba4997ab0fceecf7f8562
alanpcao@gmail.com <alanpcao@gmail.com>
2020-02-10 20:23:11 -0800

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,16 @@ from cactus import *
 from loadSprites import *
 from cloud import *
 
-def game_controller():
-    screen_size = (width,height)=(600, 150)
-    background_col = (235, 235, 235)
+screen_size = (width, height)=(600, 150)
+background = (235, 235, 235)
+clock = pygame.time.Clock()
+FPS = 60
 
+pygame.init()
+pygame.display.set_caption("T-Rex Run")
+display_surface = pygame.display.set_mode(screen_size)
+
+def game_controller():
     pygame.init()
     pygame.display.set_caption("T-Rex Run")
 
@@ -27,18 +33,75 @@ def game_controller():
         else:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    game_quit = True;
+                    game_quit = True
 
-            display_surface.fill(background_col);
+            display_surface.fill(background)
             pygame.display.update()
 
         if game_quit:
-            break;
+            break
 
         # update sprites
     pygame.quit()
     quit()
 
+def intro_screen():
+    intro_dino = Dino(44, 47) # Initializes intro screen with a dino
+    gameStart = False # Game won't start until key press
+
+    # Loads the callout image and its dimensions
+    callout, callout_rect = load_image('call_out.png', 196, 45) 
+    callout_rect.left = width * 0.05
+    callout_rect.top = height * 0.4
+
+    # Loads the ground sprite
+    intro_ground,intro_ground_rect = load_sprites('ground.png', 15, 1, -1, -1)
+    intro_ground_rect.left = width / 20
+    intro_ground_rect.bottom = height
+
+    # Loads the logo image and its dimensions
+    logo,logo_rect = load_image('logo.png', 240, 40)
+    logo_rect.centerx = width * 0.6
+    logo_rect.centery = height * 0.6
+    while not gameStart:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return True
+            if event.type == pygame.KEYDOWN: # On a key press ...
+                if event.key == pygame.K_SPACE or event.key == pygame.K_UP: # If spacebar or up arrow is pressed then dino jumps
+                    intro_dino.isJumping = True 
+                    gameStart = False # CHANGE TO TRUE WHEN FINISHED WITH gameplay()
+                    intro_dino.movement[1] = -1 * intro_dino.jumpSpeed # Modifies y movement for dinosaur jumping
+
+        intro_dino.update() # Dino will update when action is triggered
+
+        # Intro screen initialized
+        screen.fill(background) 
+        screen.blit(intro_ground[0], intro_ground_rect)
+        screen.blit(logo,logo_rect)
+        screen.blit(callout, callout_rect)
+        intro_dino.draw()
+
+        pygame.display.update() # Presents GUI
+
+        clock.tick(FPS)
+
+def disp_gameOver_msg(retry_image, gameover_image):
+    retry_image_rect = retry_image.get_rect()
+    retry_image_rect.centerx = width / 2
+    retry_image_rect.top = height * 0.52
+
+    gameover_rect = gameover_image.get_rect()
+    gameover_rect.centerx = width / 2
+    gameover_rect.centery = height * 0.35
+
+    screen.blit(retry_image, retry_image_rect)
+    screen.blit(gameover_image, gameover_rect)
+
+def main():
+    isGameQuit = intro_screen()
+    #if not isGameQuit:
+        #gameplay()
 
 game_controller()
-
+main()
