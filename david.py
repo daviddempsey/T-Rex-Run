9597a322247918f4a0db925c25e42ed66125edb6
David Dempsey <davidgdempsey@gmail.com>
2020-02-10 19:15:08 -0800

    finished ptera

--- david.py
+++ david.py
@@ -22,7 +22,7 @@ pygame.display.set_caption("T-Rex Rush")
 
 
 def load_sprites(sheet_name, sprites_horiz, sprites_vert, scale_x = -1,
-                 scale_y = -1, colorkey = None):
+                 scale_y = -1):
     sheet_path = os.path.join('sprites', sheet_name) # stores path
     spritesheet = pygame.image.load(sheet_path) # loads spritesheet
     spritesheet = spritesheet.convert() # converts image type
@@ -45,10 +45,8 @@ def load_sprites(sheet_name, sprites_horiz, sprites_vert, scale_x = -1,
             sprite = sprite.convert() # converts image type
             sprite.blit(spritesheet, (0,0), sprite_rect) # draws sprite out
 
-            if colorkey is not None:
-                if colorkey == -1:
-                    colorkey = sprite.get_at((0,0)) # color at bottom right
-                sprite.set_colorkey(colorey, RLEACCEL)
+            colorkey = sprite.get_at((0,0)) # color at bottom right
+            sprite.set_colorkey(colorey, RLEACCEL)
 
             if scale_x != -1 or scale_y != -1: # scales appropriately
                 sprite = pygame.transform.scale(sprite, (scale_x, scale_y))
@@ -59,17 +57,17 @@ def load_sprites(sheet_name, sprites_horiz, sprites_vert, scale_x = -1,
 
     return sprites, sprite_size # returns all sprites and size of each sprite
 
+
 class Dino():
     def __init__(self, size_x = -1, size_y = -1):
         # loads standing dinosaur sprites
-        self.dinos, self.dinosize = load_sprites('dino.png', 5, 1, size_x, \
-                                                 size_y, -1)
+        self.dinos, self.dinorect = load_sprites('dino.png', 5, 1, size_x, \
+                                                 size_y)
         # loads ducking dinosaur sprites
         self.duckingdinos, self.ducksize = load_sprites('dino_ducking.png',\
-                                                        2, 1, 59, size_y,\
-                                                        -1)
-        self.dinosize.bottom = int(0.98*height) # sets bottom y-pos ?
-        self.dinosize.left = width/15 # sets left pos of dinosaur
+                                                        2, 1, 59, size_y)
+        self.dinorect.bottom = int(0.98*height) # sets bottom y-pos ?
+        self.dinorect.left = width/15 # sets left pos of dinosaur
 
         self.dino = self.dinos[0] # initial dinosaur sprite
 
@@ -87,17 +85,17 @@ class Dino():
         self.jumpSpeed = 11.5
 
         # stores width of ducking and standing dino
-        self.standing_width = self.dinosize.width
+        self.standing_width = self.dinorect.width
         self.duck_width = self.ducksize.width
 
     # draws out dino on screen
     def draw(self):
-        screen.blit(self.dino, self.dinosize)
+        screen.blit(self.dino, self.dinorect)
 
     # prevents dino from jumping out of bounds
     def checkbounds(self):
-        if self.dinosize.bottom > int(0.98*height):
-            self.dinosize.bottom = int(0.98*height)
+        if self.dinorect.bottom > int(0.98*height):
+            self.dinorect.bottom = int(0.98*height)
             self.isJumping = False
 
     def update(self):
@@ -125,13 +123,13 @@ class Dino():
 
         if not self.isDucking:
             self.sprite = self.dinos[self.index] # adjusts sprite accordingly
-            self.dinosize.width = self.standing_width
+            self.dinorect.width = self.standing_width
         else:
             # adjusts sprite to ducking sprite
             self.sprite = self.duckingdinos[(self.index) % 2]
-            self.dinosize.width = self.duck_width
+            self.dinorect.width = self.duck_width
 
-        self.dinosize = self.dinosize.move(self.movement) # moves dino accordingly # Changed dino to dinosize -Alan :)
+        self.dinorect = self.dinorect.move(self.movement) # moves dino accordingly # Changed dino to dinorect -Alan :)
         self.checkbounds() # ensures dino stays on screen
 
         if not self.isDead and self.counter % 7 == 6 \
@@ -141,4 +139,54 @@ class Dino():
                 if pygame.mixer.get_init() != None:
                     checkPoint_sound.play()
 
-        self.counter = (self.counter + 1) # increases time counter by 1
+        self.counter += 1 # increases time counter by 1
+
+
+class Cactus(pygame.sprite.Sprite):
+    def __init__(self, speed=5, size_x = -1, size_y = -1):
+        pygame.sprite.Sprite.__init__(self, self.containers) # creates sprite
+        self.cacti, self.cactusrect = load_sprites('cacti-small.png', 3,
+                                                       1, sizex, sizey)
+        self.cactusrect.bottom = int(0.98*height) # positions cacti on ground
+        self.cactusrect.left = width + self.cactusrect.width # pos off screen
+        self.cactus = self.cacti[random.randrange(0,3)] # random sprite
+        self.movement = [-1*speed, 0] # moves left
+
+    def draw(self):
+        screen.blit(self.cactus, self.cactusrect) # draws cactus on screen
+
+    def update(self):
+        self.cactusrect = self.cactusrect.move(self.movement) # moves cactus
+
+        if self.cactusrect.right < 0: # deletes if moved off screen
+            self.kill()
+
+
+class Ptera(pygame.sprite.Sprite):
+    def __init__(self, speed=5, sizex=-1, sizey=-1):
+        pygame.sprite.Sprite.__init__(self.self.containers)
+        self.pteras, self.pterarect = load_sprites('ptera.png', 2, 1, size_x,
+                                                   size_y)
+        # various possible heights for pteradactyl
+        self.ptera_height = [height*0.82, height*0.75, height*0.60]
+        self.pterarect.centery = self.ptera_height[random.randrange(0,3)]
+
+        self.pterarect.left = width + self.pterarect.width # pos off screen
+        self.ptera = self.pteras[0] # sets initial ptera sprite
+        self.movement = [-1*speed, 0] # sets movement speed
+        self.index = 0
+        self.counter = 0
+
+    def draw(self):
+        screen.blit(self.ptera, self.pterarect)
+
+    def update(self):
+        if self.counter % 10 == 0:
+            self.index = (self.index+1)%2
+        # makes wings flap by changing sprite
+        self.ptera = self.pteras[self.index]
+        self.pterarect = self.pterarect.move(self.movement)
+        self.counter += 1
+        if self.pterarect.right < 0:
+            self.kill() # removes ptera if out of bounds
+
