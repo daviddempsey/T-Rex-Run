08ed5260418677236387ecb0eccead70a2662d2e
David Dempsey <davidgdempsey@gmail.com>
2020-02-29 16:06:08 -0800

    ahhhh

--- controller.py
+++ controller.py
@@ -203,48 +203,49 @@ def game_controller():
 
 
 def intro_screen():
-    intro_dino = Dino(44, 47) # Initializes intro screen with a dino
-    intro_dino.isBlinking = True
-    gameStart = False # Game won't start until key press
+    temp_dino = Dino(44,47)
+    temp_dino.isBlinking = True
+    gameStart = False
 
-    # Loads the callout image and its dimensions
-    callout, callout_rect = load_image('call_out.png', 196, 45)
-    callout_rect.left = width * 0.05
-    callout_rect.top = height * 0.4
+    callout,callout_rect = load_image('call_out.png',196,45,-1)
+    callout_rect.left = width*0.05
+    callout_rect.top = height*0.4
 
-    # Loads the ground sprite
-    intro_ground,intro_ground_rect = load_sprites('ground.png', 15, 1, -1, -1)
-    intro_ground_rect.left = width / 20
-    intro_ground_rect.bottom = height
+    temp_ground,temp_ground_rect = load_sprite_sheet('ground.png',15,1,-1,-1,-1)
+    temp_ground_rect.left = width/20
+    temp_ground_rect.bottom = height
 
-    # Loads the logo image and its dimensions
-    logo,logo_rect = load_image('logo.png', 240, 40)
-    logo_rect.centerx = width * 0.6
-    logo_rect.centery = height * 0.6
+    logo,logo_rect = load_image('logo.png',240,40,-1)
+    logo_rect.centerx = width*0.6
+    logo_rect.centery = height*0.6
     while not gameStart:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return True
-            if event.type == pygame.KEYDOWN: # On a key press ...
-                if event.key == pygame.K_SPACE or event.key == pygame.K_UP: # If spacebar or up arrow is pressed then dino jumps
-                    intro_dino.isJumping = True
-                    intro_dino.isBlinking = False
-                    intro_dino.movement[1] = -1 * intro_dino.jumpSpeed # Modifies y movement for dinosaur jumping
+        if pygame.display.get_surface() == None:
+            print("Couldn't load display surface")
+            return True
+        else:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return True
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE or event.key == pygame.K_UP:
+                        temp_dino.isJumping = True
+                        temp_dino.isBlinking = False
+                        temp_dino.movement[1] = -1*temp_dino.jumpSpeed
 
-        intro_dino.update() # Dino will update when action is triggered
+        temp_dino.update()
 
-        # Intro screen initialized
-        screen.fill(background_color)
-        screen.blit(intro_ground[0], intro_ground_rect)
-        if intro_dino.isBlinking:
-            screen.blit(logo,logo_rect)
-            screen.blit(callout, callout_rect)
-        intro_dino.draw()
+        if pygame.display.get_surface() != None:
+            screen.fill(background_col)
+            screen.blit(temp_ground[0],temp_ground_rect)
+            if temp_dino.isBlinking:
+                screen.blit(logo,logo_rect)
+                screen.blit(callout,callout_rect)
+            temp_dino.draw()
 
-        pygame.display.update() # Presents GUI
+            pygame.display.update()
 
         clock.tick(FPS)
-        if intro_dino.isJumping == False and intro_dino.isBlinking == False:
+        if temp_dino.isJumping == False and temp_dino.isBlinking == False:
             gameStart = True
 
 
